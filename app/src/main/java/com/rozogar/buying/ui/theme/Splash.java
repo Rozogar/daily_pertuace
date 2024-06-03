96977c9a78bac5984afdd9ca870d2c1ebabafb9e
Rozogar <arozogar@gmail.com>
2024-06-02 23:58:22 -0700

    App is ready to write code 03/03/13

--- app/src/main/java/com/rozogar/buying/ui/theme/Splash.java
+++ app/src/main/java/com/rozogar/buying/ui/theme/Splash.java
@@ -1,6 +1,8 @@
 package com.rozogar.buying.ui.theme;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,16 +13,18 @@ import androidx.core.view.WindowInsetsCompat;
 import com.rozogar.buying.R;
 
 public class Splash extends AppCompatActivity {
-
+Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    handler.postDelayed(new Runnable() {
+        @Override
+        public void run() {
+            Intent intent = new Intent(Splash.this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }, 2000);
     }
 }
